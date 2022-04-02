f6e512386c89fe35f0d59788a8027da6e445f129
anacarolinar21 <e107ti@cps.sp.gov.br>
2022-04-01 22:08:47 -0300

    finalizar Idade

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int idade = 35;
+if (idade <= 12)
+{
+    Console.WriteLine("Criança");
+}
+else if (idade <= 17)
+{
+    Console.WriteLine("Adolescente");
+}
+else if (idade < 65)
+{
+    Console.WriteLine("Adulto");
+}
+else{
+    Console.WriteLine("Idoso");
+}
